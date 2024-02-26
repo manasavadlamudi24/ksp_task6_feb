145f80c0f453ce66d6dbbbf4f010d00a4c6c3f73
Manasa Vadlamudi <144939761+manasavadlamudi24@users.noreply.github.com>
2024-02-26 23:30:04 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,43 @@
+##python basics
+
+print("hello world")
+##camelcase
+iamVeryBasic="zero"
+print(iamVeryBasic)
+num1=5
+num2=5.6867
+str="session"
+print(type(num1))
+print(type(str))
+print(int(num2))
+print(float(num1))
+##casting
+print(float(99))
+##indexing
+index="welocome to the session"
+print(index[0])
+print(index[1:5])
+##operators
+a = True
+b = False
+print(a and b) 
+print(a or b) 
+print(not a) 
+
+a = 10
+b = 20
+c = a 
+  
+print(a is not b) 
+print(a is c)
+
+##loops
+count = 0
+while (count < 3):
+    count = count + 1
+    print("Hello code")
+
+n = 4
+for i in range(0, n):
+    print(i)
+
